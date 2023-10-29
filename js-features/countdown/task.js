18bf795032ab9ee9efb5898689738db0cb6ba848
Irina Kriuchkova <ivk2005@yandex.ru>
2023-10-29 17:25:31 +0300

    Задание 1.1 Обратный таймер - повышенный уровень сложности #1

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,12 +1,16 @@
 const timerCount = document.getElementById("timer");
 const maxValue = timerCount.textContent;
+let timerValue = new Date(0, 0, 0, 0, 0, maxValue);
 
-let countdown = function(timerCount) {
-  timerCount.textContent -= 1;
-  if (timerCount.textContent == 0) {
+let countdown = function() {
+  timerValue.setTime(timerValue.getTime() - 1000);
+  timerCount.textContent = timerValue.toLocaleTimeString("ru-Ru", { hour: "2-digit", minute: "2-digit", second: "2-digit" });
+  if (timerValue.getSeconds() === 0) {
     alert('Вы победили в конкурсе!');
   } 
 } 
   
-let timerId = setInterval(countdown, 1000, timerCount);
+let timerId = setInterval(countdown, 1000);
 setTimeout(() => clearInterval(timerId), maxValue * 1000);
+
+
