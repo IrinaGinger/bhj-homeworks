4697e7b6e22049423cf3d750fd386d445890bc13
Irina Kriuchkova <ivk2005@yandex.ru>
2023-10-29 18:00:41 +0300

    Задание 1.1 Кликер - Повышенный уровень сложности

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,8 +1,17 @@
 const counter = document.getElementById("clicker__counter");
+const averageClickSpeed = document.getElementById("click__speed");
 const image = document.getElementById("cookie");
+let previousClick = new Date();
+let recentClick;
+let clickTime;
 
 function clickEvent() {
   let counterValue = Number(counter.textContent);
+
+  recentclick = new Date();
+  clickTime = (recentclick.getTime() - previousClick.getTime()) / 1000;
+  averageClickSpeed.textContent = (1 / clickTime).toFixed(2);
+  previousClick = recentclick;
   
   if (counterValue % 2 === 0) { 
     image.width += 20;
