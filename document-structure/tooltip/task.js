190e4766c4f6368da1b5fe621ebfbd67b9c859e9
Irina Kriuchkova <ivk2005@yandex.ru>
2023-11-30 21:47:38 +0300

    Внесено исправление по замечаниям (задание Подсказки)

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,5 +1,6 @@
 const tooltipElements = Array.from(document.getElementsByClassName("has-tooltip"));
 let activeIndex;
+document.querySelector('body').setAttribute("style", "position: relative");
 
 tooltipElements.forEach((elem, index) => {
   let tooltip = document.createElement("div");
@@ -7,20 +8,19 @@ tooltipElements.forEach((elem, index) => {
   
   tooltip.classList.add("tooltip");
   tooltip.textContent = tooltipText;
-
-  elem.setAttribute("style", "position: relative")
-  elem.appendChild(tooltip);
+  
+  elem.insertAdjacentHTML('afterEnd', tooltip.outerHTML);
 
   elem.addEventListener("click", (event) => {
     event.preventDefault();
     let position = elem.getBoundingClientRect();
 
-    if (activeIndex != undefined) {
-      tooltipElements[activeIndex].firstChild.nextSibling.classList.remove("tooltip_active");
+    if ((activeIndex != undefined) && (activeIndex != index)) {
+      tooltipElements[activeIndex].nextSibling.classList.remove("tooltip_active");
     }
     
-    elem.firstChild.nextSibling.setAttribute("style", "position: absolute; top: 16; left: 0");
-    elem.firstChild.nextSibling.classList.add("tooltip_active");
+    elem.nextSibling.setAttribute("style", `position: absolute; top: ${position.bottom}; left: ${position.left}`);
+    elem.nextSibling.classList.toggle("tooltip_active");
     activeIndex = index;
   })
 })
@@ -29,15 +29,14 @@ tooltipElements.forEach((elem, index) => {
 window.addEventListener('scroll', function() {  
   function isVisible(pos) {
       const viewportHeight = window.innerHeight;
-        
-      return ((pos.top < viewportHeight) && (pos.bottom > 0)) ? true : false;
+      return ((pos.top < viewportHeight) && (pos.bottom > 0));
     }
 
     tooltipElements.forEach(element => {
       let position = element.getBoundingClientRect();
 
       if (!isVisible(position)) {
-        element.firstChild.nextSibling.classList.remove("tooltip_active");
+        element.nextSibling.classList.remove("tooltip_active");
       }
     });
   })
